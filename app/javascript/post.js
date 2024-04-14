d49a28514c561f3373fddcbdb070b72c4c4f57a8
i-v-mii <i.v.jiji.mii@gmail.com>
2024-04-14 23:04:56 +0900

    メモ投稿機能

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -1,13 +1,9 @@
-function post (){
+function post() {
   const form = document.getElementById("form");
   form.addEventListener("submit", () => {
-    e.preventDefault();
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
-    XHR.send(formData);
   });
-};
-
-window.addEventListener('turbo:load', post);
+}
