f694f39b09f476759064f734487399713dc0786f
i-v-mii <i.v.jiji.mii@gmail.com>
2024-04-14 23:24:02 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content])
-    render json:{ post: post}
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
   end
 end
